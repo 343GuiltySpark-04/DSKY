6f9e20353fa2c443eff160b839bb951b451d7023
captain-rainbow <67162230+captain-rainbow@users.noreply.github.com>
2021-01-22 19:04:32 +0100

    Update display.ino
    pushed the wrong file lol

--- Code/display/display.ino
+++ Code/display/display.ino
@@ -1,8 +1,3 @@
-String R1D5;
-String R1D4;
-String R1D3;
-String R1D2;
-String R1D1;
 int latchPin = 11;
 int clockPin = 9;
 int dataPin = 12;
@@ -10,7 +5,15 @@ byte first;
 byte second;
 byte third;
 
+char R1D1;
+char R1D2;
+char R1D3;
+char R1D4;
+char R1D5;
 
+
+const byte numChars = 64;
+char receivedChars[numChars];
 
 void updateShiftRegister(){
   digitalWrite(latchPin, LOW);
@@ -31,17 +34,56 @@ void setup(){
   
 }
 
-void loop() {
-   recvWithStartEndMarkers();
-   dowork();
+void setNumber(char num){
+  if (num == '1'){
+    third = 0b01100000;
+    second = 0b01100000;
+  }
 
+  if (num == '2'){
+    third = 0b11011011;
+    second = 0b11011011;
+  }
+
+  if (num == '3'){
+    third = 0b11110011;
+    second = 0b01100000;
+  }
+
+  if (num == '4'){
+    third = 0b01100111;;
+    second = 0b01100111;;
+  }
+
+  if (num == '5'){
+    third = 0b10110111;
+    second = 0b10110111;
+  }
+
+  if (num == '6'){
+    third = 0b10111111;
+    second = 0b10111111;
+  }
+
+  if (num == '7'){
+    third = 0b11100001;
+    second = 0b11100001;
+  }
+  if (num == '8'){
+    third = 0b11111111;
+    second = 0b11111111;
+  }
+  if (num == '9'){
+    third = 0b11100111;
+    second = 0b11110111;
+  }
+  if (num == '0'){
+    third = 0b11111101;
+    second = 0b11111101;
+  }
 }
 
-//===============
-const byte numChars = 64;
-char receivedChars[numChars];
 
-boolean newData = false;
 void recvWithStartEndMarkers() {
     static boolean recvInProgress = false;
     static byte ndx = 0;
@@ -49,7 +91,7 @@ void recvWithStartEndMarkers() {
     char endMarker = '>';
     char rc;
 
-    while (Serial.available() > 0 && newData == false) {
+    while (Serial.available() > 0) {
         rc = Serial.read();
 
         if (recvInProgress == true) {
@@ -64,7 +106,7 @@ void recvWithStartEndMarkers() {
                 receivedChars[ndx] = '\0'; // terminate the string
                 recvInProgress = false;
                 ndx = 0;
-                newData = true;
+                
             }
         }
 
@@ -74,64 +116,22 @@ void recvWithStartEndMarkers() {
     }
 }
 
-void setNumber(String num){
-  if (num == 1){
-    third = 0b01100000;
-    second = 0b01100000;
-  }
-
-  if (num == 2){
-    third = 0b11011011;
-    second = 0b11011011;
-  }
-
-  if (num == 3){
-    third = 0b11110011;
-    second = 0b01100000;
-  }
-
-  if (num == 4){
-    third = 0b01100111;;
-    second = 0b01100111;;
-  }
-
-  if (num == 5){
-    third = 0b10110111;
-    second = 0b10110111;
-  }
-
-  if (num == 6){
-    third = 0b10111111;
-    second = 0b10111111;
-  }
-
-  if (num == 7){
-    third = 0b11100001;
-    second = 0b11100001;
-  }
-  if (num == 8){
-    third = 0b11111111;
-    second = 0b11111111;
-  }
-  if (num == 9){
-    third = 0b11100111;
-    second = 0b11110111;
-  }
-  if (num == 0){
-    third = 0b11111101;
-    second = 0b11111101;
-  }
+void loop(){
+  recvWithStartEndMarkers();
+  ControlDisplay();  
+  
 }
 
-void dowork(){
-  R1D5=receivedChars[7];
-  R1D4=receivedChars[7];
-  R1D3=receivedChars[6];
-  R1D2=receivedChars[4];
-  R1D1=receivedChars[2];
-  Serial.write(R1D1[2]); 
+void ControlDisplay(){
+  
+  R1D5=receivedChars[5];
+  R1D4=receivedChars[3];
+  R1D3=receivedChars[2];
+  R1D2=receivedChars[1];
+  R1D1=receivedChars[0];
+   
   first = 0b01111111;
-  setNumber(R1D1);
+  setNumber(R1D5);
   updateShiftRegister();
   delay(1);
   first = 0b10111111;
@@ -147,7 +147,8 @@ void dowork(){
   updateShiftRegister();
   delay(1);
   first = 0b11110111;
-  setNumber(R1D5);
+  setNumber(R1D1);
   updateShiftRegister();
   delay(1);
+ 
 }
